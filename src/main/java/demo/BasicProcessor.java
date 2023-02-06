61a6316b372199e2ce42cc2a260edf1e9c7febb0
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-02-07 00:05:56 +0900

    Update Processor

--- src/main/java/demo/BasicProcessor.java
+++ src/main/java/demo/BasicProcessor.java
@@ -3,6 +3,11 @@ package demo;
 public class BasicProcessor {
 
 	public static void main(String[] args) {
+		System.out.printf("Args.Length: %s %n", args.length);
+		for (String arg : args) {
+			System.out.printf("Arg[x]: %s %n", arg);
+		}
+		System.out.printf("Env.APP_ENV: %s %n", System.getenv("APP_ENV"));
 		try {
 			for (int i = 0; i < 10; i++) {
 				System.out.println("Foo " + i);
